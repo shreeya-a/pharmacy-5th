75809ece4e4c8025a1fc7d0b9ec684cbdfd1eb12
shreeya-a <shreeyadhikari11@gmail.com>
2022-12-08 23:04:34 +0545

    initial setup with bootstrap

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+@extends('layout.app')
+
+@section('content')
+
+
+<!-- <div class="container"> -->
+    <h1 style="text-align:center ;">Welcome to Pharmacy website</h1>
+
+
+<!-- </div> -->
+@endsection
