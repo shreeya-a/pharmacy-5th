8f4ef1fd707238361e72972a69a3d7670a350f3c
Anurag4734 <anuragkarki4734@gmail.com>
2023-02-22 13:30:08 +0545

    prescription  ui updated

--- resources/views/image/create.blade.php
+++ resources/views/image/create.blade.php
@@ -2,27 +2,9 @@
 
 @section('content')
 
-{{-- <form action="/image" method="post" enctype="multipart/form-data">
-    @csrf
-    <div>
-        <label for="email">Title:</label>
-        <input type="email" name="email" id="email" value="{{ old('email') }}">
-        @error('email')
-            <div>{{ $message }}</div>
-        @enderror
-    </div>
-    <div>
-        <label for="image">Image:</label>
-        <input type="file" name="image" id="image">
-        @error('image')
-            <div>{{ $message }}</div>
-        @enderror
-    </div>
-    <button type="submit">Upload</button>
-</form> --}}
 
 
-{{-- <div class="container mt-3">
+<div class="container mt">
     <div class="row">
         <div class="col-md-12">
             <div class="card">
@@ -52,7 +34,7 @@
                             <div class="col-md-6">
                                 <div class="custom-file">
                                     <input type="file" class="custom-file-input @error('image') is-invalid @enderror" id="image" name="image">
-                                    <label class="custom-file-label" for="image">{{ __('Choose file...') }}</label>
+                                    {{-- <label class="custom-file-label" for="image">{{ __('Choose file...') }}</label> --}}
 
                                     @error('image')
                                         <span class="invalid-feedback" role="alert">
@@ -75,60 +57,9 @@
             </div>
         </div>
     </div>
-</div> --}}
-
-<section style="margin-bottom: 100px" class="vh-100">
-    <div class="container h-100">
-        <div class="row d-flex justify-content-center align-items-center h-100">
-            <div class="col-lg-12 col-xl-11 mt-30">
-                <div class="card text-black mt-30" style="border-radius:0;">
-                    <div class="card">
-                        <div class="card-header">{{ __('Add New Image') }}</div>
-                        <div class="card-body p-md-5">
-                                <form method="POST" action="{{ route('image.store') }}" enctype="multipart/form-data">
-                                    @csrf
-            
-                                    <div class="d-flex flex-row align-items-center mb-4">
-                                        <i class="fas fa-envelope fa-lg me-3 fa-fw"></i>
-                                        <div class="form-outline flex-fill mb-0">
-                                        <label for="email" class="form-level">{{ __('Email') }}</label>
-            
-                                        <div class="col-md-6">
-                                            <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-            
-                                            @error('email')
-                                                <span class="invalid-feedback" role="alert">
-                                                    <strong>{{ $message }}</strong>
-                                                </span>
-                                            @enderror
-                                        </div>
-                                    </div>
-            
-                                    <div class="form-group row">
-                                        <label for="image" class="col-md-4 col-form-label text-md-right">{{ __('Image') }}</label>
-            
-                                        <div class="col-md-6">
-                                            <div class="custom-file">
-                                                <input type="file" class="custom-file-input @error('image') is-invalid @enderror" id="image" name="image">
-                                                <label class="custom-file-label" for="image">{{ __('Choose file...') }}</label>
-            
-                                                @error('image')
-                                                    <span class="invalid-feedback" role="alert">
-                                                        <strong>{{ $message }}</strong>
-                                                    </span>
-                                                @enderror
-                                            </div>
-                                        </div>
-                                    </div>
-            
-                                    <div class="form-group row mb-0">
-                                        <div class="col-md-6 offset-md-4">
-                                            <button type="submit" class="btn btn-primary">
-                                                {{ __('Save') }}
-                                            </button>
-                                        </div>
-                                    </div>
-                                </form>
+</div>
 
 
+
+                            
 @endsection
