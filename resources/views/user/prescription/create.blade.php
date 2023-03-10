b32c23c4d81a9bddc9ab87501324811d7829fbd0
shreeya-a <shreeyadhikari11@gmail.com>
2023-03-10 20:59:01 +0545

    prescription upload validated

--- resources/views/user/prescription/create.blade.php
+++ resources/views/user/prescription/create.blade.php
@@ -3,7 +3,6 @@
 @section('content')
 
 
-
 <div class="container my-5">
     <div class="row">
 
@@ -15,7 +14,7 @@
             </div>
             @endif
             <div class="card-header">{{ __('Upload prescription') }}</div>
-            <div class="col-md-7">
+            <div class="col-md-12">
 
                 <div class="card-body ">
 
@@ -26,52 +25,75 @@
                             <div class="col-md-6">
                                 <label for="">First Name</label>
                                 <input type="text" value="{{ Auth::user()->name}}" name="fname" class="form-control" placeholder="Enter First Name" readonly>
+
                             </div>
                             <div class="col-md-6">
                                 <label for="">Last Name</label>
                                 <input type="text" value="{{ Auth::user()->lname}}" name="lname" class="form-control" placeholder="Enter Last Name">
+
                             </div>
 
                             <div class="col-md-6 mt-3">
                                 <label for="">Email</label>
                                 <input type="text" value="{{ Auth::user()->email}}" name="email" class="form-control" placeholder="Enter Email" readonly>
+
                             </div>
                             <div class="col-md-6 mt-3">
                                 <label for="">Phone Number</label>
-                                <input type="text" value="{{ Auth::user()->phone}}" name="phone" class="form-control" placeholder="Enter Phone Number">
+                                <input type="text" value="{{ Auth::user()->phone}}" name="phone" class="form-control  @error('phone') is-invalid @enderror" placeholder="Enter Phone Number">
+                                @error('phone')
+                                <span class="invalid-feedback mt-2" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
                             </div>
                             <div class="col-md-6 mt-3">
                                 <label for="">Address</label>
-                                <input type="text" value="{{ Auth::user()->address}}" name="address" class="form-control" placeholder="Enter Address ">
+                                <input type="text" value="{{ Auth::user()->address}}" name="address" class="form-control  @error('address') is-invalid @enderror" placeholder="Enter Address ">
+                                @error('address')
+                                <span class="invalid-feedback mt-2" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
                             </div>
                             <div class="col-md-6 mt-3">
                                 <label for="">City</label>
-                                <input type="text" value="{{ Auth::user()->city}}" name="city" class="form-control" placeholder="Enter City">
+                                <input type="text" value="{{ Auth::user()->city}}" name="city" class="form-control  @error('city') is-invalid @enderror" placeholder="Enter City">
+                                @error('city')
+                                <span class="invalid-feedback mt-2" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
                             </div>
 
                             <div class="col-md-6 mt-3">
                                 <label for="">Country</label>
-                                <input type="text" value="{{ Auth::user()->country}}" name="country" class="form-control" placeholder="Enter Country">
+                                <input type="text" value="{{ Auth::user()->country}}" name="country" class="form-control  @error('country') is-invalid @enderror" placeholder="Enter Country">
+                                @error('country')
+                                <span class="invalid-feedback mt-2" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
                             </div>
                         </div>
                         <!-- <div class="form-group row">
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Email') }}</label> -->
 
-                        <div class="col-md-6">
-                            <!-- <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus> -->
-                            <!-- 
+                        <!-- <div class="col-md-6"> -->
+                        <!-- <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus> -->
+                        <!-- 
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror -->
-                        </div>
+                        <!-- </div> -->
                         <div class=" col-md-6 mt-3">
 
                             <!-- <div class="col-md-6"> -->
                             <label for="image" class="">Image</label>
                             <div class="custom-file">
-                                <input type="file" class="form-control custom-file-input @error('image') is-invalid @enderror" id="image" name="image">
+                                <input type="file" class="form-control custom-file-input @error('image') is-invalid @enderror" id="image" name="image" onchange="loadFile(event)">
                                 {{-- <label class="custom-file-label" for="image">{{ __('Choose file...') }}</label> --}}
 
                                 @error('image')
@@ -91,17 +113,12 @@
                 </div>
                 </form>
             </div>
-            <div class="col-md-5">
-                            <div>
-                                <img id="output" width="400" height="350" src="https://booster.io/wp-content/uploads/product-add-to-cart-e1438362099361.png">
-                            </div>
-                        </div>
+
+
+
+
         </div>
-
-
-
     </div>
-</div>
 </div>
 </div>
 
