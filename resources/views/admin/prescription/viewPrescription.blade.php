0ad020290618a670006b3fbe8b350256cfc0e4e3
shreeya-a <shreeyadhikari11@gmail.com>
2023-03-25 01:46:10 +0545

    hyperlink

--- resources/views/admin/prescription/viewPrescription.blade.php
+++ resources/views/admin/prescription/viewPrescription.blade.php
@@ -144,7 +144,7 @@
                 </div>
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
-                        <li class="breadcrumb-item"><a href="#">Home</a></li>
+                    <li class="breadcrumb-item"><a href="{{url('/dashboard')}}">Home</a></li>
                         <li class="breadcrumb-item active">Prescription</li>
                         <li class="breadcrumb-item active">Order</li>
 
