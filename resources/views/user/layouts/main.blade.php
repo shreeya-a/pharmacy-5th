d20d0b74f0edbabd6079448c812f89e80e6ddf67
shreeya-a <shreeyadhikari11@gmail.com>
2023-04-20 19:28:28 +0545

    landing page complete

--- resources/views/user/layouts/main.blade.php
+++ resources/views/user/layouts/main.blade.php
@@ -112,21 +112,7 @@
 
     <!-- <script src="https://code.jquery.com/jquery-3.6.0.js"></script> -->
     <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"></script>
-    <script>
-    var availableTags = [];
-    $.ajax({
-        method: 'GET',
-        url: "/search-product",
-        success: function(response){
-            startAutoComplete(response);
-        }
-    });
-    function startAutoComplete(availableTags){
-        $( "#search_product" ).autocomplete({
-        source: availableTags
-        });
-    }
-  </script>
+
 
     @yield('scripts')
 
