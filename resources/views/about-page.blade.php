f5fc2c474dd6c9e12ed918849d90a0cd0e9b5386
shreeya-a <shreeyadhikari11@gmail.com>
2023-04-21 00:00:51 +0545

    about us

--- resources/views/about-page.blade.php
+++ resources/views/about-page.blade.php
@@ -7,9 +7,26 @@
       color: black;
    }
 </style>
+<section class="breadcrumbs-wrapper pt-2 pb-2">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="breadcrumbs-style breadcrumbs-style-1 d-md-flex justify-content-between align-items-center">
+                    <div class="breadcrumb-left">
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item"><a href="{{route('homepage')}}">Home</a></li>
+                            <li class="breadcrumb-item active" aria-current="page">Contact Us</li>
+                        </ol>
+                    </div>
+                  
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 <div class="container my-5">
    
-    <h1 style="text-align:left;">About Us</h1><br>
+    <h1 style="text-align:center;">About Us</h1><br>
     <p><strong> NePharma</strong> is an online portal for medicine store . We place ethical values
        in the forefront while executing our activities. Ever since it’s establishment , we have been 
        focusing on serving not only the patient of the hospital but also to the nearby communities. We are 
