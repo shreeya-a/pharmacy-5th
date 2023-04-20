bc57f9ce966dd0474667d265b3cecbee628905ae
shreeya-a <shreeyadhikari11@gmail.com>
2023-04-20 23:59:23 +0545

    contact us

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,8 +1,33 @@
 @extends('user.layouts.main')
 
 @section('content')
+<style>
+    h1,p{
+        color: black;
+    }
+    .form-label{
+        color: black;
+    }
+</style>
 
-<h1 class="text-center">Contact Us</h1>
+<section class="breadcrumbs-wrapper pt-2 pb-2">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="breadcrumbs-style breadcrumbs-style-1 d-md-flex justify-content-between align-items-center">
+                    <div class="breadcrumb-left">
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item"><a href="{{route('homepage')}}">Home</a></li>
+                            <li class="breadcrumb-item active" aria-current="page">Contact Us</li>
+                        </ol>
+                    </div>
+                  
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<h1 class="text-center mt-3">Contact Us</h1>
 
 
 <section style="margin-bottom: 100px" class="vh-100">
@@ -88,4 +113,7 @@
         </div>
     </div>
 </section>
+@endsection
+@section('scripts')
+<script src="{{asset('userpanel/assets/js/custom.js')}}"></script>
 @endsection
